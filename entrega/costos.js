2a8d2c72083e761883a4720a218992e9c0756d93
Sant3x <117774288+Sant3x@users.noreply.github.com>
2023-07-11 17:35:54 -0300

    Add files via upload

--- entrega/costos.js
+++ entrega/costos.js
@@ -1,12 +1,42 @@
+let mail = prompt('Cual es tu mail?');
+ 
+ 
+
+const options1 = { style: 'currency', currency: 'USD' };
+const numberFormat1 = new Intl.NumberFormat('en-US', options1);
+ 
+
+
+
 function costos() {
-    var costoluz = document.getElementById("luz").value;
-    var costogas = document.getElementById('gas').value;
-    var internet = document.getElementById('internet').value;
-    var celular = document.getElementById('celular').value;
-    var supermercado = document.getElementById('supermercado').value;
-    var sueldo = document.getElementById("dinero").value;
+
   
-    result.innerHTML  = parseFloat(costoluz) + parseFloat(costogas) + parseFloat(internet) + parseFloat(celular) + parseFloat(supermercado);
-    result1.innerHTML  = parseFloat(sueldo) - parseFloat(costoluz) - parseFloat(costogas) - parseFloat(internet) - parseFloat(celular) - parseFloat(supermercado);
+  let costoluz = document.getElementById("luz").value;
+  let costogas = document.getElementById('gas').value;
+  let internet = document.getElementById('internet').value;
+  let celular = document.getElementById('celular').value;
+  let supermercado = document.getElementById('supermercado').value;
+  let sueldo = document.getElementById("dinero").value;
+  let alquiler = document.getElementById("alquiler").value;
+  let tarjeta = document.getElementById("tarjeta").value;
+  let salidas = document.getElementById("salidas").value;
+  let sueldoscostos = parseFloat(sueldo) - parseFloat(costoluz) - parseFloat(costogas) - parseFloat(internet) - parseFloat(celular) - parseFloat(supermercado);
+
+
+
+
+
+  let sumatatoria = (parseFloat(tarjeta) + parseFloat(alquiler) + parseFloat(costoluz) + parseFloat(costogas) + parseFloat(internet) + parseFloat(celular) + parseFloat(supermercado) + parseFloat(salidas))
+  let salidascostos = (salidas / sueldo).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
+  let costos = (sumatatoria / sueldo).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
+  result.innerHTML = (numberFormat1.format(sumatatoria));
+  result2.innerHTML = (costos);
+  result1.innerHTML = (numberFormat1.format(sueldoscostos));
+  result3.innerHTML = (salidascostos);
+  result4.innerHTML = (getRawData);
+
 }
+
+
+
 
