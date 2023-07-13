8380b47bfcc697d8655a492c575d6b4fa9ef454d
Sant3x <117774288+Sant3x@users.noreply.github.com>
2023-07-13 12:00:51 -0300

    Add files via upload

--- entrega/costos.js
+++ entrega/costos.js
@@ -1,16 +1,34 @@
-let mail = prompt('Cual es tu mail?');
- 
- 
+const validarEmail = (email) => {
+  if (!isNaN(email) || email === null || email === '' || (email !== null && email.trim() === '') || email.length < 5 || !email.includes('@') || !email.includes('.com')) {
+    alert('Por favor, introduzca un email válido con al menos 5 caracteres y que contenga "@" y ".com".');
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const pedirMail = () => {
+  let mail = prompt('Cuál es tu mail?');
+
+  if (validarEmail(mail)) {
+    confirm('Genial, continuemos!');
+  } else {
+    pedirMail();
+  }
+};
+
+pedirMail();
+
+
+
+
 
 const options1 = { style: 'currency', currency: 'USD' };
 const numberFormat1 = new Intl.NumberFormat('en-US', options1);
  
 
-
-
 function costos() {
 
-  
   let costoluz = document.getElementById("luz").value;
   let costogas = document.getElementById('gas').value;
   let internet = document.getElementById('internet').value;
@@ -25,18 +43,16 @@ function costos() {
 
 
 
-
-  let sumatatoria = (parseFloat(tarjeta) + parseFloat(alquiler) + parseFloat(costoluz) + parseFloat(costogas) + parseFloat(internet) + parseFloat(celular) + parseFloat(supermercado) + parseFloat(salidas))
+  let sumatatoria = (parseFloat(tarjeta) + parseFloat(alquiler) + parseFloat(costoluz) + parseFloat(costogas) + parseFloat(internet) + parseFloat(celular) + parseFloat(supermercado))
   let salidascostos = (salidas / sueldo).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
   let costos = (sumatatoria / sueldo).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
   result.innerHTML = (numberFormat1.format(sumatatoria));
   result2.innerHTML = (costos);
   result1.innerHTML = (numberFormat1.format(sueldoscostos));
   result3.innerHTML = (salidascostos);
-  result4.innerHTML = (getRawData);
+  result4.innerHTML = (numberFormat1.format(salidas));
 
 }
-
 
 
 
